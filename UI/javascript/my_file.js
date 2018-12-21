580d37aeb4724323198701192e5f527bd0219829
lizz24 <39797444+lizz24@users.noreply.github.com>
2018-12-21 10:25:27 +0300

    Create my_file.js

---
+++ UI/javascript/my_file.js
@@ -0,0 +1,19 @@
+
+function addButton(){
+    var but = document.getElementById('but').value;
+    
+        
+           window.location.href= "interven.html";
+          
+
+    }
+
+function clickButton(){
+    var but = document.getElementById('but').value;
+    
+        
+           window.location.href= "add_report.html";
+          
+
+    }
+
